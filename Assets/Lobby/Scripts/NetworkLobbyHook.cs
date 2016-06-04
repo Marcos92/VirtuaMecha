c813b3f8ab06edc1e36a75a0da7acb505c762fe7
Pina <jmpi_jmpi@hotmail.com>
2016-06-04 16:22:20 +0100

    Network Update

---
+++ Assets/Lobby/Scripts/NetworkLobbyHook.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Prototype.NetworkLobby;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.UI;
+
+public class NetworkLobbyHook : LobbyHook
+{
+    // Used to pass variables from the lobby to the game
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        //LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+        //NetworkLobbyInfo info = gamePlayer.GetComponent<NetworkLobbyInfo>();
+
+        //info.lobbyIndex = lobby.slot;
+        //Debug.Log("Lobby index = " + info.lobbyIndex);
+        //info.characterIndex = lobby.characterIndex;
+        //Debug.Log("Character index = " + info.characterIndex);
+        //info.playerName = lobby.playerName;
+        //Debug.Log("Name index = " + info.playerName);
+    }
+}
---
+++ Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ControllerManager : MonoBehaviour
+{
+    [HideInInspector]
+    public static ControllerManager instance;
+
+    public GameObject[] Buttons;
+
+    private EventSystem eventSystem;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+        ChangeFocus(0);
+    }
+
+    public void ChangeFocus(int index)
+    {
+        eventSystem.firstSelectedGameObject = Buttons[index];
+    }
+}
